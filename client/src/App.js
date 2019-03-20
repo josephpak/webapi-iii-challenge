0497301be886847f61cfae19516bb4759eac3ed0
joepak <josephpak2014@gmail.com>
2019-03-20 18:50:13 -0400

    Setting up routing for App

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from "react-router-dom";
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Switch>
+          <Route exact path="/users" component={UsersList}/>
+          <Route exact path="/posts" component={PostsList}/>
+          <Route exact path="/users/:id"
+          render={props => <User {...props} />} 
+          />
+          <Route exact path="/users/:id/posts"
+          render={props => <UserPosts {...props} />} 
+          />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     );
   }
